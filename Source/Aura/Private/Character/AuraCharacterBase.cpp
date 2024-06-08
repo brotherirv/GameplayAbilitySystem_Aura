9944536bf07d230d716efb8424d2b56d4a5acaa6
Irvin Castillo <irvin1024@yahoo.com>
2024-06-08 16:05:25 -0700

    Added character and enemy class

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,23 @@
+// Copyright Druid Mechanics
+
+
+#include "Character/AuraCharacterBase.h"
+
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+ 	
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+
+
