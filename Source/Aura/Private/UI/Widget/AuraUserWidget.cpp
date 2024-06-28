011c5174f4ceb1bfb8a3600a9696116af9b41d2a
Irvin Castillo <irvin1024@yahoo.com>
2024-06-28 16:15:00 -0700

    Created a controller class for the Widgets based off of object, and widget class. Created a Widget Blueprint based off of that class. In blueprints, created what the widget blueprint will look like. Then implemented blueprint function to change the appearance of the image being used like opacity and such. The WBP is a base for creating other Widgets.

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,12 @@
+// Copyright Druid Mechanics
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
+
+
