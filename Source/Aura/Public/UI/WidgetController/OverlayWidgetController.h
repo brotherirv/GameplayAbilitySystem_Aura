5c7a20f94c04b5589d1493ab539b8ce96aebf683
Irvin Castillo <irvin1024@yahoo.com>
2024-08-07 15:26:39 -0700

    We are creating a a data table that has information related to the gameplay tags. More specifically for showing messages to the screen. We made a USTRUCT called FUIWidgetRow, and it derives from FTableRowBase. FTableRowBase is the base class for a row structure for data tables. We want to show a widget class to the screen, and create a TSubclassOf. We go and create a data table in the editor based off of The UIWidgetRow that we created. We create new tags for messages. We then create a variable of the MessageWidgetData table inside of our OverlayWidgetController header. We make it UProperrty, and set it in the editor.

--- Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
@@ -4,12 +4,33 @@
 
 #include "CoreMinimal.h"
 #include "UI/WidgetController/AuraWidgetController.h"
+#include <UI/Widget/AuraUserWidget.h>
+#include "GameplayTagContainer.h"
 #include "OverlayWidgetController.generated.h"
+
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthChangedSignature, float, NewHealth);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxHealthChangedSignature, float, NewMaxHealth);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnManaChangedSignature, float, NewMana);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxManaChangedSignature, float, NewMaxMana);
+
+USTRUCT(BlueprintType)
+struct FUIWidgetRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+		UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		FGameplayTag MessageTag = FGameplayTag();
+
+		UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		FText Message = FText();
+
+		UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		TSubclassOf<UAuraUserWidget> MessageWidget;
+
+		UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		UTexture2D* Image = nullptr;
+};
 
 
 struct FOnAttributeChangeData;
@@ -37,6 +58,9 @@ public:
 	FOnMaxManaChangedSignature OnMaxManaChanged;
 
 protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Widget Data")
+	TObjectPtr<UDataTable> MessageWidgetDataTable;
+
 	void HealthChanged(const FOnAttributeChangeData& Data) const;
 	void MaxHealthChanged(const FOnAttributeChangeData& Data) const;
 	void ManaChanged(const FOnAttributeChangeData& Data) const;
