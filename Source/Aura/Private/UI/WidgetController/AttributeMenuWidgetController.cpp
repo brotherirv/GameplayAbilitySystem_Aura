30d8b83f0441da3108a10795325711289f121dd3
Irvin Castillo <irvin1024@yahoo.com>
2024-09-06 00:13:46 -0700

    91- We create an AbilitySystemLibrary based off of the BlueprintFunctionLibrary in cpp. Function libraries usually have static functions. We put a GetOverlayWidgetController function. It has an input for UObject that we call world context object.  We put that input because in order for us to get the playercontroller, we have to call a ugameplaystatics function that requires the uobject worldcontextobjext. We use the player controller to get PS, ASC, and AS.We also cast the aura hud, get the playerstate, AbilitySystemComponent, And Attributeset. Then we call a const function FWidgetControllerParams that takes all of those as inputs. We end up returning GetOverlayWidgetController.

--- Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -3,3 +3,10 @@
 
 #include "UI/WidgetController/AttributeMenuWidgetController.h"
 
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+}
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+}
