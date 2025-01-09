08ecf199ddb7304fe41444cf013e1a17c4553300
Irvin Castillo <irvin1024@yahoo.com>
2025-01-08 20:31:10 -0800

    We adjust the AbilityInputTagReleased function. We check for the leftMouseButton being clicked. If we are, we check if it's a short press. If it is, then we're creating spline path for the character to go through. We use a functino called FindPathToLocationSynchronously. It takes in world context, pathstart, and the destination of the end of the path. We run a for loop that loops through the vectors in our Navpath. For each vector, we add a spline point. And then we're drawing a debug sphere, and then switch the bAutoRunning bool to true. We're clearing the spline points before running this function, that way it creates new spline points each time. We reset bTageting to false and FollowTime to 0. We add a NavMeshBoundsVolume in the editor. The DebugSpheres are working, but the character isn't automatically moving to the location after one click yet.

--- Source/Aura/Player/AuraPlayerController.cpp
+++ Source/Aura/Player/AuraPlayerController.cpp
@@ -9,6 +9,9 @@
 #include "AuraGameplayTags.h"
 #include <AbilitySystemBlueprintLibrary.h>
 #include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "NavigationSystem.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "NavigationPath.h"
 
 
 AAuraPlayerController::AAuraPlayerController()
@@ -22,6 +25,7 @@ void AAuraPlayerController::PlayerTick(float DeltaTime)
 {
 	Super::PlayerTick(DeltaTime);
 	CursorTrace();
+
 }
 
 void AAuraPlayerController::BeginPlay()
@@ -144,9 +148,45 @@ void AAuraPlayerController::AbilityInputTagPressed(FGameplayTag InputTag)
 
 void AAuraPlayerController::AbilityInputTagReleased(FGameplayTag InputTag)
 {
-	if (GetASC() == nullptr) return;
-	GetASC()->AbilityInputTagReleased(InputTag);
-}
+	if (!InputTag.MatchesTagExact(FAuraGameplayTags::Get().InputTag_LMB))
+	{
+		if (GetASC())
+		{
+			GetASC()->AbilityInputTagReleased(InputTag);
+		}
+		return;
+	}
+		if (bTargeting)
+		{
+			if (GetASC())
+			{
+				GetASC()->AbilityInputTagReleased(InputTag);
+			}
+		}
+		else
+		{
+			APawn* ControlledPawn = GetPawn();
+			if (FollowTime <= ShortPressThreshold && ControlledPawn)
+			{
+				if (UNavigationPath* NavPath = UNavigationSystemV1::FindPathToLocationSynchronously(this, ControlledPawn->GetActorLocation(), CachedDestination))
+				{
+					Spline->ClearSplinePoints();
+					for (const FVector& PointLoc : NavPath->PathPoints)
+					{
+						Spline->AddSplinePoint(PointLoc, ESplineCoordinateSpace::World);
+						DrawDebugSphere(GetWorld(), PointLoc, 8.f, 8, FColor::Green, false, 5.f);
+					}
+					bAutoRunning = true;
+				}
+			}
+			FollowTime = 0.f;
+			bTargeting = false;
+		}
+		
+	}
+
+
+
 
 void AAuraPlayerController::AbilityInputTagHeld(FGameplayTag InputTag)
 {
