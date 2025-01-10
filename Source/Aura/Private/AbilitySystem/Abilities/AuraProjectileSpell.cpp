a349742065f2478b77610dd187678c81be3b3526
Irvin Castillo <irvin1024@yahoo.com>
2025-01-10 02:30:37 -0800

    We created a projectile spell class based off of AuraGameplayAbility class. We then put the ActivateAbility function in it. In the CPP, we printed out a string. Then we created a blueprint, and gave it a print string message when event activate ability. In order to link up the Ability, we set up a startup input tag to the LMB from the GAFirebolt blueprint. Inside of the AuraCharacter Blueprint, we gabe it the GAFirebolt ability as a startup ability. Now when you left click a character, the messages are printed out.

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -0,0 +1,14 @@
+// Copyright Druid Mechanics
+
+
+#include "AbilitySystem/Abilities/AuraProjectileSpell.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+    const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+    const FGameplayEventData* TriggerEventData)
+{
+    Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+    UKismetSystemLibrary::PrintString(this, FString("ActivateAbilit (C++)"), true, true, FLinearColor::Yellow, 3);
+}
