011d95df49ed2154901667f57c6befa01641ef62
Irvin Castillo <irvin1024@yahoo.com>
2025-01-17 17:49:40 -0800

    We created the Animation Montage. Inside of the Animation Montage, We have a notify. We chose the AN_MontageEvent that we created from the parent AnimNotify. This AN_MontageEvent uses the function SendGameplayEventToActor, and takes in an Event Tag GameplayTag Variable. When we Put the AN_MontageEvent Inside of our AnimMontage, we have the opportunity to set the EventTag that we added in AN_MontageEvent. GA_FireBolt fires PlayMontageAndWait, where it fires out the AnimationMontage. We add WaitGameplayEvent, Where it takes in EventMontageFirebolt tag. In C++, we added the code for spawning the projectile in a new functino that is blueprint callable. We then call it from GA_FireBolt when the GameplayEvent is received, that way the spawn syncs with the event. We finish this off by calling the EndAbility. That allows us to be able to call the GameplayAbility again.

--- Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -12,7 +12,15 @@ void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Hand
 {
     Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 
-	const bool bIsServer = HasAuthority(&ActivationInfo);
+	
+
+ 
+}
+
+void UAuraProjectileSpell::SpawnProjectile()
+{
+	
+	const bool bIsServer = GetAvatarActorFromActorInfo()->HasAuthority();
 	if (!bIsServer) return;
 	ICombatInterface* CombatInterface = Cast<ICombatInterface>(GetAvatarActorFromActorInfo());
 	if (CombatInterface)
@@ -32,6 +40,4 @@ void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Hand
 
 		Projectile->FinishSpawning(SpawnTransform);
 	}
-
- 
 }
