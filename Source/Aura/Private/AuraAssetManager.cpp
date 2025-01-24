999e74e749b83e939e753c6e72d0498394a892a7
Irvin Castillo <irvin1024@yahoo.com>
2025-01-23 18:29:51 -0800

    We're receiving the target data. Inside the activate function, we sent it from client to server. We use the ServerSetReplicatedTarget Data to replicate targeting data to the server. We know that it then gets sent from cleint to server because the function is being called if bIsLocallyControlled. The target data reaches the server, and there's a delegate broadcast from the server. That's where we check with the function ShouldBroadcastAbilityTaskDelegates. Now we're checking for the else statement. If we're not locally controlled, then we're listening for the target data. We're using the AbilityTargetDataSetDelegate function to get the delegate for a  given Ability/PredictionKey pair. That's what the previous ServerSetReplicatedTargetData function took in.  In order for AbilityTargetDataSetDelegate to know that information, We're calling the functions GetAbilitySpecHandle() and GetActivationPredictionKey(). We use add UObject in order to bind the OnTargetDataReplicatedCallback. We do that by passing in this, and the newly created OnTargetDataReplicated callback function. The reason that we pass on this, is because this class we're writing the code in is where the function is. OnTargetDataReplicatedCallback will broadcast the DataHandle and the ActivationTag. Inside the functino, we call from ASC ConsumeClientReplicatedTargetData. It consumes the cache TargetData from the client, Inside the function, we're passing GetAbilitySpecHandle() and GetActivationPredictionKey(). This information ends up getting broadcasted. Now, bellow the AddUObjectm we create a bool called bCalledDelegate. It calls a Get function for CallReplicatedTargetDataDelegatesIfSet function. If any of those are void, that means that the server hasn't received the information yet. That means that it shouldn't broadcast anything yet. Then the bool will be false and We will call a funchen the task is ended prematurely.

--- Source/Aura/Private/AuraAssetManager.cpp
+++ Source/Aura/Private/AuraAssetManager.cpp
@@ -3,6 +3,7 @@
 
 #include "AuraAssetManager.h"
 #include "AuraGameplayTags.h"
+#include "AbilitySystemGlobals.h"
 
 UAuraAssetManager& UAuraAssetManager::Get()
 {
@@ -17,5 +18,8 @@ void UAuraAssetManager::StartInitialLoading()
     Super::StartInitialLoading();
 
     FAuraGameplayTags::InitializeNativeGameplayTags();
+
+    //This is required to use TargetData
+    UAbilitySystemGlobals::Get().InitGlobalData();
 
 }
