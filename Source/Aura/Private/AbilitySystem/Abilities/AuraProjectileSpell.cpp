11a1fadc19abfe797161831fabdbc199e0b9b036
Irvin Castillo <irvin1024@yahoo.com>
2025-02-13 20:21:56 -0800

    134. Instead of hard coding the damage from the blueprint, we're hard coding it inside of AuraProjectileSpell.cpp. We do this by getting the gameplay tags, and using tthe AssignTagSetByCallerMagniture. The function takes in a spechandle, a data tag, and a float to represent damage. We pass wour spec handle, we pass in from our AuraGameplayTag that we created in AuraGameplayTags called Damage, ad pass in the number 50. Inside of the editor, the gameplay effect is changed to set by caller. This is what allows for the cpp file to apply the damage. In set by caller magniture drop down, it takes a data tag. We just have to input damage, and now the character is doing the damage from the hardcoded cpp file.

--- Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -6,6 +6,7 @@
 #include "AbilitySystemComponent.h"
 #include "Interaction/CombatInterface.h"
 #include <AbilitySystemBlueprintLibrary.h>
+#include "Aura/Public/AuraGameplayTags.h"
 
 
 void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
@@ -46,6 +47,10 @@ void UAuraProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocati
 		// Gives the Projectile a Gameplay Effect Spec for causing Damage.
 		const UAbilitySystemComponent* SourceASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(GetAvatarActorFromActorInfo());
 		const FGameplayEffectSpecHandle SpecHandle = SourceASC->MakeOutgoingSpec(DamageEffectClass, GetAbilityLevel(), SourceASC->MakeEffectContext());
+		
+		FAuraGameplayTags GameplayTags = FAuraGameplayTags::Get();
+		UAbilitySystemBlueprintLibrary::AssignTagSetByCallerMagnitude(SpecHandle, GameplayTags.Damage, 50.f);
+
 		Projectile->DamageEffectSpecHandle = SpecHandle;
 		Projectile->FinishSpawning(SpawnTransform);
 	}
