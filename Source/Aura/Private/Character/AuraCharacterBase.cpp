efa690c75187affb72faee5ab376cbf3a82f081c
Irvin Castillo <irvin1024@yahoo.com>
2025-02-20 22:16:43 -0800

    -139.We're creating a dissolve effect when the enemy dies. In order to do this, we're creating a dynamic material instance that can have its parameters dynamically changed at runtime. Inside of AuraCharacterBase, we create a UMaterialInstance for the DissolveCharacter and Weapon. We also create a Dissolve function that will swap out the material. It takes an input of UMaterialInstanceDynamic. Inside of the Dissolve function, we check if the UMaterialInstances are valid. If they are, we're creating UMaterialInstanceDynamic by inputing the UMaterialInstances. Then we set the material of the mesh with the newly created UMaterialInstanceDynamic. We call StartDissolveTimeLine and StartDWeaponDisolveTimeline and also add to the input the UMaterialInstanceDynamic. We then call the Dissolve function inside of the MulticastHandleDeath_Implementation function. Inside of EnemyBaseBP, I call the events that StartsDisolveTimeline, and add timelines to them. The timelines Then I call SetScalarParameterValue function. Inside of the timelines, I set values that show the items and when they've disappeared. After doing so, the character should dissolve when killed. Mine aren't. But that's what should be happening. May fix in future commit.

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -52,6 +52,8 @@ void AAuraCharacterBase::MulticastHandleDeath_Implementation()
 	GetMesh()->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Block);
 
 	GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	
+	Dissolve();
 
 }
 
@@ -98,6 +100,24 @@ void AAuraCharacterBase::AddCharacterAbilities()
 	if (!HasAuthority()) return;
 
 	AuraASC->AddCharacterAbilities(StartupAbilities);
+}
+
+void AAuraCharacterBase::Dissolve()
+{
+	if (IsValid(DissolveMaterialInstance))
+	{
+		UMaterialInstanceDynamic* DynamicMatInst = UMaterialInstanceDynamic::Create(DissolveMaterialInstance, this);
+		GetMesh()->SetMaterial(0, DynamicMatInst);
+
+		StartDissolveTimeline(DynamicMatInst);
+	}
+	if (IsValid(WeaponDissolveMaterialInstance))
+	{
+		UMaterialInstanceDynamic* DynamicMatInst = UMaterialInstanceDynamic::Create(WeaponDissolveMaterialInstance, this);
+		GetMesh()->SetMaterial(0, DynamicMatInst);
+
+		StartWeaponDissolveTimeline(DynamicMatInst);
+	}
 }
 
 
