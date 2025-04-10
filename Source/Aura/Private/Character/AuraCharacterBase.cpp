ef449f99c8c80a454efc203536e5847c9457cc29
Irvin Castillo <irvin1024@yahoo.com>
2025-04-09 22:57:22 -0700

    185. TAGGED MONTAGE. We want to get the montage and associated montage tag to know which socket to look for. We're gonna make a struct that links the montage and tag. We're gonna make it in the combat interface. It has an UPROPERTY's for AnimMontage and FGameplayTag. Inside of AuraCharacterBase, we make sure to have CombatInterface included. We make a UPROPERTY for a TArray that takes in the newly created FTaggedMontage struct, and call it AttackMontages. The combat interface will have a function that will retrieves the Attack Montages. It will be a UFUNCTION that is a BlueprintNativeEvent, and BlueprintCallable. It will be a TArray of FTaggedMontage. We're calling it in AuraCharacterBase, calling virtual and override.It will return TArray called AttackMontages. Now inside of our character blueprints like in BP_Goblin_Spear, we can go into Attack Montages and Set the Montage and the Montage Tag. Inside of GA_Melee attack, We're now calling GetAttackMontages.We pass in GetAvatarActorFromActorInfo. On the return value, we return the length of the array and get Random Interget in Range. It will get a number between the length of the array and 0. Then we use Get to get the ref from the array. We use BreakTaggedMontage. It gives us an pin for Montage and Montage Tag. We use those pins to connect to PlayMontageAndWait, and WaitGameplayEvent. We will go into the Animation Montage for the Attack_GoblinSpear and change the event tag to the newly created tag.

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -81,6 +81,10 @@ AActor* AAuraCharacterBase::GetAvatar_Implementation()
 {
 	return this;
 }
+TArray<FTaggedMontage> AAuraCharacterBase::GetAttackMontages_Implementation()
+{
+	return AttackMontages;
+}
 void AAuraCharacterBase::InitAbilityActorInfo()
 {
 }
