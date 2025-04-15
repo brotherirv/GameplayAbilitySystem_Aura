c6e88ac0574154e5e368dc9de4b80f916ff3fb2f
Irvin Castillo <irvin1024@yahoo.com>
2025-04-15 16:36:43 -0700

    196. SPAWNING THE ROCK PROJECTILE. We're spawning the the rock projectile from the ranged attack. Inside of the GA_RangedAttack, we're calling GetCombatTarget, and passing that through spawn Projectile. We add effects to make the rock appear more clearly. We also go into the BP_Rock and add a spin OnEventPlay. We also go into the projectile CPP. Inside the OnSphereOverlap, we check for the IsNotFriend function that we made in UAuraAbilitySystemLibrary. We get inside of the DamageEffectSpecHandle, and get the effect causer.The second input is the OtherActor. If they're friends, the function will return. If they're not friends, it will run the rest of the function.

--- Source/Aura/Private/Actor/AuraProjectile.cpp
+++ Source/Aura/Private/Actor/AuraProjectile.cpp
@@ -11,6 +11,7 @@
 #include <AbilitySystemBlueprintLibrary.h>
 #include "AbilitySystemComponent.h"
 #include "NiagaraFunctionLibrary.h"
+#include <AbilitySystem/AuraAbilitySystemLibrary.h>
 
 
 // Sets default values
@@ -80,6 +81,10 @@ void AAuraProjectile::Destroyed()
 void AAuraProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	if (DamageEffectSpecHandle.Data.IsValid() && DamageEffectSpecHandle.Data.Get()->GetContext().GetEffectCauser() == OtherActor)
+	{
+		return;
+	}
+	if (!UAuraAbilitySystemLibrary::IsNotFriend(DamageEffectSpecHandle.Data.Get()->GetContext().GetEffectCauser(), OtherActor))
 	{
 		return;
 	}
