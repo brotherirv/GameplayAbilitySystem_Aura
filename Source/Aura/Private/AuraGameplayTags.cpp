907bd4bd73b9b34c6f7255e46951b4ae87e4778f
Irvin Castillo <irvin1024@yahoo.com>
2025-05-02 16:07:25 -0700

    221. SUMMONMING PARTICLE EFFECT. We're calling the SpawnSystemAtLocation function inside of GA_SummonAbility to spawn the GroundSummon Particle. We're running it through the for each loop. 222. SELECT MINION CLASS AT RANDOM. We're creating a blueprint pure function that returns a random index from MinionClasses array. We call it in our GA_SummonAbility. We also call SpawnActorFromClass. For now, we manually put the character up. We need to assign a player controller to the SpawnActor, so we call SpawnDefaultController. 223. MINION SUMMON MONTAGE. We are creating an animation montage for shaman summon. We're adding a montage event. We're adjusting the position. We want our minions to be facing away from the shaman. We're calling GetAvatarActorFromActorInfo. Using that to GetActorLocation. Then we're calling FindLookAtRotation. The Start input will come from the ActorLocation. The Target will be the minion location. 224. MINION COUNT. We're summoning way too many minions. We need a minion threshold. We want to create a summon tag, so our shaman can summon and attack. We create the tag, and we create an int32 to keep track of the threshold. In Combat Interface, we create a GetMinionCount() function. We define it in our CharacterBase class, and add implementation. It returns MinionCount. We then set the ability to summon. 225. ELEMENTALIST BEHAVIOR TREE.  We are creating a behavior tree that is specific to the shaman. We set EnemyFireBolt inside of startup abilities.

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -65,6 +65,7 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 	//Abilities
 
 	GameplayTags.Abilities_Attack = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Abilities.Attack"), FString("Attack Abilities Tag"));
+	GameplayTags.Abilities_Summon = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Abilities.Summon"), FString("Summon Abilities Tag"));
 
 	//Combat Sockets
 
--- Source/Aura/Public/Interaction/CombatInterface.h
+++ Source/Aura/Public/Interaction/CombatInterface.h
@@ -74,5 +74,8 @@ public:
 
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
 	FTaggedMontage GetTaggedMontageByTag(const FGameplayTag& MontageTag);
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	int32 GetMinionCount();
 	
 };
