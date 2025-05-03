1cccf51560cce049d285f721308fed6aea976cc7
Irvin Castillo <irvin1024@yahoo.com>
2025-05-03 15:59:26 -0700

    226. ELEMENTALIST ATTACK TASK. We're working on our Elementalist EnemyBehaviorTree. We want our attack to check how many minions are in the map. If they're lower than a certain number, we want to activate the spawn ability. Because of that, we're going to duplicate our BTT. we're creating a GameplayTag variable called SummonTag. We're giving it a value of Abilities.Summon. We're also creating an integer for MinionSpawnThreshhold. We're checking if minion count is less than the minion spawn threshold. Depending if it's true or false, we're setting the ability tag to either summon tag or attack tag. This is great, but we need to keep track of the minions in the world. In our CombatInterface, we're making a function to increment minion count. We implement it inside of AuraCharacterBase. If the value needs to be decremented, we will pass a negative value. We will call IncrementMinionCount inside of our GA_SummonAbility. Now we have a minion count. We have a function that gets the minion count. We have a function that increments the minion count. We check if the minion count is less than a threshold.

--- Source/Aura/Public/Interaction/CombatInterface.h
+++ Source/Aura/Public/Interaction/CombatInterface.h
@@ -77,5 +77,8 @@ public:
 
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
 	int32 GetMinionCount();
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void IncrementMinionCount(int32 Amount);
 	
 };
