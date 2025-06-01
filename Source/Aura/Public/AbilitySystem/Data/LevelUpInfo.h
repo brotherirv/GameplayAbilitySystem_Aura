63d18ab6584501d65a6befb71f65769acf7573db
Irvin Castillo <irvin1024@yahoo.com>
2025-05-31 17:01:55 -0700

    250. LevelUpInfoDataAsset. We created a DataAsset and called it LevelUpInfoDataAsset. We added a struct that has values for Leveling up, Attribute Point Rewards, and Spell Point Rewards. We created a TArray that holds the structs for LevelUpInfo. We create a function that gets the XP, and sets your level according to how much XP you have. We then created this DataAsset in blueprints and set the values for the struct members.

---
+++ Source/Aura/Public/AbilitySystem/Data/LevelUpInfo.h
@@ -0,0 +1,40 @@
+// Copyright Druid Mechanics
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "LevelUpInfo.generated.h"
+
+/**
+ * 
+ */
+
+USTRUCT(BlueprintType)
+struct FAuraLevelUpInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 LevelUpRequirement = 0;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	int32 AttributePointReward = 1;
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 SpellPointReward = 0;
+
+	
+};
+UCLASS()
+class AURA_API ULevelUpInfo : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly)
+	TArray<FAuraLevelUpInfo> LevelUpInformation;
+
+	int32 FindLevelForXP(int32 XP);
+	
+};
